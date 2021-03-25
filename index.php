50120e9da072394bda846a071e39d52090a16fee
angel-alvarez-guzman <67205127+angel-alvarez-guzman@users.noreply.github.com>
2021-03-25 15:33:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 
 require_once "clases/conexion/conexion.php";
-
+/*
 $conexion = new conexion();
 
 $query = "INSERT INTO usuarios(nombre, usuario, password, perfil, foto, estado, ultimo_login, fecha) VALUES ('alvarez', 'alvarez', '123', 'Especial', '',1, '0000-00-00 00:00:00', now())";
 
 print_r($conexion->nonQueryId($query));
+*/
 
 ?>
 
-hola index
+
